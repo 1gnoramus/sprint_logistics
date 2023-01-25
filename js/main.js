4c9038fe0607b39d53d15638f72c67121cb12e94
1gnoramus <elenwarface@gmail.com>
2023-01-25 23:28:13 +0600

    +js fetch data

--- js/main.js
+++ js/main.js
@@ -1,18 +1,47 @@
 
-    document.getElementById('send_request_button').addEventListener('click', function(){
+    // const init = function(){
+    //     document.getElementById('close').addEventListener('click',reset)
+    //     document.getElementById('send_request_button').addEventListener('click',send)
+
+    // }
+
+    // const reset = function(ev){
+    //     // функция возвращает форму в начальное положение/удаляет данные с полей ввода
+    //     ev.preventDefault();
+    //     document.getElementById('form').reset();
+    // }
+
+    // const send = function(ev){
+    //     ev.preventDefault();
+        
+    // }
+
+    function take_value(){
+        var username = document.getElementById('username').value;
+        var phone = document.getElementById('phone').value;
+        document.write(username);
+        document.write(phone);
+    }
+
+    document.getElementById('send_request_button').addEventListener('click', 
+    function(){
     document.querySelector('.bg-modal').style.display='none'});
 
 
     document.getElementById('button').addEventListener('click', 
     function(){
         document.querySelector('.bg-modal').style.display='flex';
-        document.querySelector
     });
+
     document.getElementById('button2').addEventListener('click', 
     function(){
         scrollToTop();
         document.querySelector('.bg-modal').style.display='flex';
-        document.querySelector
+    });
+
+    document.getElementById('close').addEventListener('click', 
+    function(){
+        document.querySelector('.bg-modal').style.display='none'
     });
 
     function onclicked(){
@@ -39,4 +68,9 @@ function scrollToTop() {
         window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
         scrollAnimation = setTimeout("scrollToTop()", 10);
     } else clearTimeout(scrollAnimation);
+}
+
+function othername() {
+    var input = document.getElementById("username").value;
+    alert(input);
 }
