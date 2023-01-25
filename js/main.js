3dad3335d38e241bedaa81acacfba7dc1d4cecd6
1gnoramus <elenwarface@gmail.com>
2023-01-26 00:20:17 +0600

    code ref

--- js/main.js
+++ js/main.js
@@ -17,37 +17,53 @@
     // }
 
     function take_value(){
+        // ФУНКЦИЯ ДЛЯ ПОЛУЧЕНИЯ ИНПУТА С ЗАПРОСА ЗВОНКА
         var username = document.getElementById('username').value;
         var phone = document.getElementById('phone').value;
         document.write(username);
         document.write(phone);
     }
 
+    // ОТПРАВИТЬ ЗАПРОС И ЗАКРЫТЬ ФОРМУ
     document.getElementById('send_request_button').addEventListener('click', 
     function(){
     document.querySelector('.bg-modal').style.display='none'});
 
-
+    // ОТКРЫТЬ ФОРМУ ЗАПРОСА ЗВОНКА
     document.getElementById('button').addEventListener('click', 
     function(){
         document.querySelector('.bg-modal').style.display='flex';
     });
 
+    // ОТКРЫТЬ ФОРМУ ЗАПРОСА ЗВОНКА
     document.getElementById('button2').addEventListener('click', 
     function(){
         scrollToTop();
         document.querySelector('.bg-modal').style.display='flex';
     });
 
+    // ПРОКРУТИТЬ СТРАНИЦУ НАВЕРХ
+    function scrollToTop() {
+        var position =
+            document.body.scrollTop || document.documentElement.scrollTop;
+        if (position) {
+            window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+            scrollAnimation = setTimeout("scrollToTop()", 10);
+        } else clearTimeout(scrollAnimation);
+    }
+
+    // ЗАКРЫТЬ ФОРМУ ЗАПРОСА ЗВОНКА
     document.getElementById('close').addEventListener('click', 
     function(){
         document.querySelector('.bg-modal').style.display='none'
     });
 
+    // ОТПРАВИТЬ ЗАПРОС НА ЗАЯВКУ
     function onclicked(){
         swal("Ваш запрос отправлен в обработку! В ближайшее время мы с Вами свяжемся!");
     }
 
+    // СВАЙПЕР ДОСТИЖЕНИЙ НА ИНДЕКСНОЙ СТРАНИЦЕ
     const swiper = new Swiper('.swiper', { 
         loop: true,
         pagination: {
@@ -61,16 +77,14 @@
     }
 );
 
-function scrollToTop() {
-    var position =
-        document.body.scrollTop || document.documentElement.scrollTop;
-    if (position) {
-        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-        scrollAnimation = setTimeout("scrollToTop()", 10);
-    } else clearTimeout(scrollAnimation);
-}
 
-function othername() {
-    var input = document.getElementById("username").value;
-    alert(input);
-}
+    // ДРУГОЙ СПОСОБ ВЫТАЩИТЬ ДАННЫЕ И ОТОБРАЗИТЬ
+    function othername() {
+        var input = document.getElementById("username").value;
+        
+        var phone = document.getElementById('phone').value;
+
+        var data= input+phone
+        
+        alert(data);
+    }
