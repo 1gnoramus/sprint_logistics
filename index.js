aa06907c149cafd6655669689d24f1ccabbb6c0e
1gnoramus <elenwarface@gmail.com>
2023-02-09 00:52:22 +0600

    its all done... ty my brother

--- index.js
+++ index.js
@@ -56,10 +56,35 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.static('assets'));
+app.use('/css', express.static(__dirname + 'assets/css'))
+app.use('/css', express.static(__dirname + 'assets/images'))
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname +'/index.html');
+  });
+  app.get('/services_lcl', (req, res) => {
     res.sendFile(__dirname +'/services_lcl.html');
   });
+  app.get('/services_avia', (req, res) => {
+    res.sendFile(__dirname +'/services_avia.html');
+  });
+  app.get('/services_ftl', (req, res) => {
+    res.sendFile(__dirname +'/services_ftl.html');
+  });
+  app.get('/services_eur', (req, res) => {
+    res.sendFile(__dirname +'/services_eur.html');
+  });
+  app.get('/services_railways', (req, res) => {
+    res.sendFile(__dirname +'/services_railways.html');
+  });
+  app.get('/index', (req, res) => {
+    res.sendFile(__dirname +'/index.html');
+  });
+  app.get('/geography', (req, res) => {
+    res.sendFile(__dirname +'/geography.html');
+  });
+
 
 
 app.post('/submit-data', (req, res) => {
